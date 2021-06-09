233ffa8a839f6b672bba51d21787d09c0dd99eb5
AYMEN <tizaouiaymen3@gmail.com>
2021-06-08 22:33:11 -0400

    configuration de git avec le TP2

---
+++ cmake-build-debug/CMakeFiles/TP_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_2.dir/main.cpp.obj"
+  "TP_2.exe"
+  "TP_2.exe.manifest"
+  "TP_2.pdb"
+  "libTP_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
